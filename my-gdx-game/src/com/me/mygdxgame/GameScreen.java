de42f9bebfd66a4337843b8e9d23ff04ca752104
Kristoffer <kristoffersilverhav@gmail.com>
2014-04-13 01:48:15 +0200

    collisionlistener

--- my-gdx-game/src/com/me/mygdxgame/GameScreen.java
+++ my-gdx-game/src/com/me/mygdxgame/GameScreen.java
@@ -14,6 +14,11 @@ import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.Manifold;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class GameScreen implements Screen {
@@ -47,6 +52,7 @@ public class GameScreen implements Screen {
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 1920, 1080);
 
+		createCollisionListener();
 	}
 
 	@Override
@@ -70,6 +76,34 @@ public class GameScreen implements Screen {
 		world.step(1 / 60f, 6, 2);
 	}
 
+	private void createCollisionListener() {
+	    world.setContactListener(new ContactListener() {
+
+	    	@Override
+	    	public void beginContact(Contact contact) {
+	    		Fixture fixtureA = contact.getFixtureA();
+	    		Fixture fixtureB = contact.getFixtureB();
+	            Gdx.app.log("beginContact", "between " + fixtureA.toString() + " and " + fixtureB.toString());
+	        }
+
+	        @Override
+	        public void endContact(Contact contact) {
+	            Fixture fixtureA = contact.getFixtureA();
+	            Fixture fixtureB = contact.getFixtureB();
+	            Gdx.app.log("endContact", "between " + fixtureA.toString() + " and " + fixtureB.toString());
+	        }
+
+	        @Override
+	        public void preSolve(Contact contact, Manifold oldManifold) {
+	        }
+
+	        @Override
+	        public void postSolve(Contact contact, ContactImpulse impulse) {
+	        }
+
+	    });
+	}
+	 
 	@Override
 	public void resize(int width, int height) {
 		// TODO Auto-generated method stub
