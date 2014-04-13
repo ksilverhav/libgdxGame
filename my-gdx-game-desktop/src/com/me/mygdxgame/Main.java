815095d8d67fa620783ac621dc85f35d13541176
Palsson123 <jacpa975@student.liu.se>
2014-04-13 16:13:20 +0200

    Screen resolution

--- my-gdx-game-desktop/src/com/me/mygdxgame/Main.java
+++ my-gdx-game-desktop/src/com/me/mygdxgame/Main.java
@@ -1,16 +1,19 @@
 package com.me.mygdxgame;
 
-import com.badlogic.gdx.Gdx;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		cfg.title = "my-gdx-game";
 		cfg.useGL20 = true;
-		cfg.width = 1000;
-		cfg.height = 500;
+		cfg.width = screenSize.width-192;
+		cfg.height = screenSize.height-108;
 		
 		new LwjglApplication(new MyGdxGame(), cfg);
 	}
